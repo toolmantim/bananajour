457189514035f423ed675f3ebfcd5aecda9c5fc7
Tim Lucas <t.lucas@toolmantim.com>
2009-05-16 04:01:25 +1000

    Switched to only showing the branch if the commit *is* the branch.

--- lib/bananajour/grit_extensions.rb
+++ lib/bananajour/grit_extensions.rb
@@ -1,5 +1,14 @@
 require 'md5'
 
+Grit::Commit.class_eval do
+  def ==(other)
+    self.id == other.id
+  end
+  def head(repo)
+    repo.heads.find {|h| h.commit == self}
+  end
+end
+
 Grit::Actor.class_eval do
   def gravatar_image
     "http://www.gravatar.com/avatar/#{MD5.md5(email.downcase)}"
