4dfecc645db8d4bc1fb03963c243f2d8b54dff96
Tony Issakov <tissakov@thefrontiergroup.com.au>
2009-05-17 11:54:00 +1000

    Updated JS to flash in the new items since last check

--- sinatra/public/javascripts/banana.js
+++ sinatra/public/javascripts/banana.js
@@ -10,10 +10,10 @@ var utility = {
 
 var banana = {
   //template for standard row
-	template_standard: '<li><p class="message"> ${message} <span class="meta">~ ${committed_date_pretty} by ${author_name} - ${nice_id}</span></p></li>',
+	template_standard: '<li class=${is_new}><p class="message"> ${message} <span class="meta">~ ${committed_date_pretty} by ${author_name} - ${nice_id}</span></p></li>',
 
   //template for row with a branch label
-	template_master: '<li><em class="branch">${head}</em><p class="message"> ${message} <span class="meta">~ ${committed_date_pretty} by ${author_name} - ${nice_id}</span></p></li>',
+	template_master: '<li class=${is_new}><em class="branch">${head}</em><p class="message"> ${message} <span class="meta">~ ${committed_date_pretty} by ${author_name} - ${nice_id}</span></p></li>',
 
   //fetch the index and use that to fetch json for each repository
 	getData : function(){
@@ -21,37 +21,54 @@ var banana = {
 			$.each(data.repositories, function(i) {
 				$.getJSON("/" + data.repositories[i].name + ".json", function(repository) {
 				  banana.ajaxUpdate(repository);
+				  $(".new").hide().fadeIn();
 				});
 			});
-		});  
+		}); 
+	},
+	
+	already_exists: function(content, search_id){
+	  return (content.indexOf(search_id)<0);
 	},
 
   //use a repository data object to populate the repository information
 	ajaxUpdate: function( /*object*/ repository) {
 		var container = $("#" + repository.html_friendly_name + " .commits");
+		var existing_content = container.get(0).innerHTML;
 		var headBranch = null;
-		var remaining = [];
+		var new_entries = [];
+		var old_entries = [];
 		$.each(repository.recent_commits, function(i) {
 		  var commit = repository.recent_commits[i];
+		  commit.author_name = commit.author.name;
+		  commit.nice_id = commit.id.substr(0, 7);
+		  commit.already_exists = banana.already_exists(existing_content, commit.nice_id);		  		  
+		  commit.is_new = (commit.already_exists)?"new":"old";
 		  var template = banana.template_standard;
 		  if (commit.head) {
 		    template = banana.template_master;
 		  }
-		  commit.author_name = commit.author.name;
-		  commit.nice_id = commit.id.substr(0, 7);
 		  var messagePara = utility.substitute(template, commit);
 		  if(commit.head){
 		    headBranch = $(messagePara);
 		  } else {
-		    remaining.push(messagePara)
+		    if(commit.already_exists){
+		      new_entries.push(messagePara);
+		    } else {
+		      old_entries.push(messagePara);
+  	    }
 		  }
 		});
-    olderCommits = $(remaining.join(""));
     //clear out previous entries
     container.empty();
     if(headBranch) {
       container.append(headBranch)
+    } 
+    if(new_entries.length>0){
+      container.append($(new_entries.join("")));
     }
-    container.append(olderCommits);
+    if(old_entries.length>0){
+      container.append($(old_entries.join("")));
+    }
 	}
 }
