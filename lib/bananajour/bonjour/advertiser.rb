aef2d9de396ce6c8b2aaf0e034881150e4e4fe7a
Tim Lucas <t.lucas@toolmantim.com>
2009-06-12 23:32:14 +1000

    Advertise the Bananjour version over Bonjour too

--- lib/bananajour/bonjour/advertiser.rb
+++ lib/bananajour/bonjour/advertiser.rb
@@ -14,6 +14,7 @@ class Bananajour::Bonjour::Advertiser
       tr["email"] = Bananajour.config.email
       tr["uri"] = Bananajour.web_uri
       tr["gravatar"] = Bananajour.gravatar
+      tr["version"] = Bananajour::VERSION
       DNSSD.register("#{Bananajour.config.name}'s bananajour", "_http._tcp,_bananajour", nil, Bananajour.web_port, tr) {}
     end
     def register_repos
@@ -43,6 +44,7 @@ class Bananajour::Bonjour::Advertiser
         tr["bjour-email"] = Bananajour.config.email
         tr["bjour-uri"] = Bananajour.web_uri
         tr["bjour-gravatar"] = Bananajour.gravatar
+        tr["bjour-version"] = Bananajour::VERSION
         service = DNSSD.register(new_repo.name, "_git._tcp,_bananajour", nil, 9418, tr) {}
         service.class.instance_eval { attr_accessor(:repository) }
         service.repository = new_repo
