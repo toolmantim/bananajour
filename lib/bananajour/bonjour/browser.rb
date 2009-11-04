1eba478bdf26669bbc441bdcc215798978c3aa89
James Sadler <freshtonic@gmail.com>
2009-11-05 07:33:48 +0800

    Another attempt to beat DNSSD::UnknownError
    I refactored browser.rb so that all of DNSSD methods are executed
    synchronously within one thread for browser.rb.  This makes it much
    easier to catch errors because I don't need to worry what thread the
    errors are occurring on - there can just be one handler within the browser
    thread.

--- lib/bananajour/bonjour/browser.rb
+++ lib/bananajour/bonjour/browser.rb
@@ -30,30 +30,31 @@ class Bananajour::Bonjour::Browser
   end
   private
     def watch!
-      DNSSD.browse(@service) do |br|
-        begin
-          Timeout.timeout(5) do
-            DNSSD.resolve(br) do |rr|
+      Thread.new do
+        while true do
+          begin
+            DNSSD.browse!(@service) do |br|
               begin
-                @mutex.synchronize do
-                  rr_exists = Proc.new {|existing_rr| existing_rr.target == rr.target && existing_rr.fullname == rr.fullname}
-                  if (DNSSD::Flags::Add & br.flags.to_i) != 0
-                    @replies << rr unless @replies.any?(&rr_exists)
-                  else
-                    @replies.delete_if(&rr_exists)
+                DNSSD.resolve!(br) do |rr|
+                  begin
+                    @mutex.synchronize do
+                      rr_exists = Proc.new {|existing_rr| existing_rr.target == rr.target && existing_rr.fullname == rr.fullname}
+                      if (DNSSD::Flags::Add & br.flags.to_i) != 0
+                        @replies << rr unless @replies.any?(&rr_exists)
+                      else
+                        @replies.delete_if(&rr_exists)
+                      end
+                    end
+                  ensure
+                    rr.service.stop unless rr.service.stopped?
                   end
                 end
-              rescue DNSSD::UnknownError
-                $stderr.puts "unknown error occurred in dnssd: #{$!.message}"
-              ensure
-                rr.service.stop unless rr.service.stopped?
               end
             end
+          rescue DNSSD::UnknownError
+            $stderr.puts "unknown error in DNSSD: '#{$!.message}'"
           end
-        rescue DNSSD::UnknownError
-          $stderr.puts "unknown error occurred in dnssd: #{$!.message}"
-        rescue Timeout::Error
-          # Do nothing
+          sleep 5
         end
       end
     end
