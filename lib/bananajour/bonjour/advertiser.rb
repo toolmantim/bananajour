9a4d1b26472dc00943aae4aa181448db9c889b2b
Tim Lucas <t.lucas@toolmantim.com>
2009-11-22 16:33:51 +1100

    [DEV] Don't need empty blocks with the latest DNSSD gem

--- lib/bananajour/bonjour/advertiser.rb
+++ lib/bananajour/bonjour/advertiser.rb
@@ -15,7 +15,7 @@ class Bananajour::Bonjour::Advertiser
       tr["uri"] = Bananajour.web_uri
       tr["gravatar"] = Bananajour.gravatar
       tr["version"] = Bananajour::VERSION
-      DNSSD.register("#{Bananajour.config.name}'s bananajour", "_http._tcp,_bananajour", nil, Bananajour.web_port, tr) {}
+      DNSSD.register("#{Bananajour.config.name}'s bananajour", "_http._tcp,_bananajour", nil, Bananajour.web_port, tr)
     end
     def register_repos
       loop do
@@ -45,7 +45,7 @@ class Bananajour::Bonjour::Advertiser
         tr["bjour-uri"] = Bananajour.web_uri
         tr["bjour-gravatar"] = Bananajour.gravatar
         tr["bjour-version"] = Bananajour::VERSION
-        service = DNSSD.register(new_repo.name, "_git._tcp,_bananajour", nil, 9418, tr) {}
+        service = DNSSD.register(new_repo.name, "_git._tcp,_bananajour", nil, 9418, tr)
         service.class.instance_eval { attr_accessor(:repository) }
         service.repository = new_repo
         @services << service
